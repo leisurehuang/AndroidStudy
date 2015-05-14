fc7c918e9eddd8c33262a024f6144f203c37db3a
Lei Huang <leisurehuang34@sina.com>
2015-05-14 18:06:27 +0800

    complite the login view logic

--- app/src/main/java/com/example/leihuang/myapplication/MainActivity.java
+++ app/src/main/java/com/example/leihuang/myapplication/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.leihuang.myapplication;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
+
+    static String E_NAME = "com.example.leihuang.Name";
+    static String E_PWD = "com.example.leihuang.Pwd";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +42,32 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void pressLoginButton(View view){
+        Intent newIntent = new Intent(this,SecondActivity.class);
+        EditText nameText = (EditText)findViewById(R.id.nameTextField);
+        String nameString = nameText.getText().toString();
+        newIntent.putExtra(E_NAME,nameString);
+
+        EditText pwdText = (EditText)findViewById(R.id.passwordTextField);
+        String pwdString = pwdText.getText().toString();
+        newIntent.putExtra(E_PWD, pwdString);
+
+        if (nameString.length() == 0 || pwdString.length() == 0) {
+            this.showErrorAlert();
+        }else {
+            startActivity(newIntent);
+        }
+
+    }
+
+    public  void showErrorAlert() {
+        Dialog alertDialog = new AlertDialog.Builder(this)
+                .setTitle("Dialog")
+                .setMessage("Please input name & password")
+                .setPositiveButton("OK",null)
+                .create();
+        alertDialog.show();
     }
 }
